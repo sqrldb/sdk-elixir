d5a9e88607841f5a9372911a095d5527092cdffb
Wess Cope <me@wess.io>
2026-01-30 17:05:07 -0500

    Adds

--- release.sh
+++ release.sh
@@ -3,17 +3,23 @@ set -e
 
 cd "$(dirname "$0")"
 
-VERSION=$(grep 'version:' mix.exs | head -1 | awk -F'"' '{print $2}')
+VERSION="0.1.0"
 
-echo "Building squirreldb Elixir SDK v${VERSION}..."
+echo "Releasing squirreldb_sdk v${VERSION}..."
 
+echo "Getting dependencies..."
 mix deps.get
-mix compile
 
 echo "Running tests..."
 mix test
 
+echo "Building docs..."
+mix docs
+
 echo "Publishing to Hex..."
 mix hex.publish
 
-echo "Published squirreldb@${VERSION} to Hex"
+echo "Released squirreldb_sdk@${VERSION}"
+echo ""
+echo "Users can install by adding to mix.exs:"
+echo "  {:squirreldb_sdk, \"~> ${VERSION}\"}"
